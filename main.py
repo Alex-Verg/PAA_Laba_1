bc10830c2caff6578b9584d5815b73092dbbc24a
Alex-Verg <alexvergelyuk@gmail.com>
2021-09-20 20:39:39 +0300

    Add and test bubble sort function

--- main.py
+++ main.py
@@ -18,6 +18,25 @@ class AnalysisArray:
     def generate_down(self, n):
         self.arr = range(n, 0, -1)
 
+    def bubble_sort(self):
+        total_swaps = 0
+        total_compares = 0
+        for i in range(len(self.arr)-1):
+            swaps = 0
+            for j in range(len(self.arr)-i-1):
+                total_compares += 1
+                if self.arr[j+1] < self.arr[j]:
+                    self.arr[j+1], self.arr[j] = self.arr[j], self.arr[j+1]
+                    swaps += 1
+            total_swaps += swaps
+            if not swaps:
+                break
+
+        return total_compares, total_swaps
+
 
 if __name__ == "__main__":
-    pass
+    test = AnalysisArray()
+    test.generate_rand(50)
+    test.bubble_sort()
+    print(*test.arr)
